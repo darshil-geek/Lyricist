778f502a6f272f7416ed28078d3f88b6690e7155
Darshil Shah <58663553+darshil-geek@users.noreply.github.com>
2021-07-05 00:12:52 +0530

    concatenated the lyrics in one string

--- lyricistbot.py
+++ lyricistbot.py
@@ -1,17 +1,18 @@
 import discord
 import lyricsgenius as genius
-import config
+#import config
 
-api = genius.Genius('')
-api = genius.Genius(config.GENIUS_TOKEN)
+api = genius.Genius('') #add genius bot token
+#api = genius.Genius(config.GENIUS_TOKEN)
 
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-TOKEN = ''
-TOKEN = config.BOT_TOKEN
+TOKEN = '' #add discord bot token
+#TOKEN = config.BOT_TOKEN
 
-client = commands.Bot(command_prefix = '-')
+client = commands.Bot(command_prefix = '.')
+client.remove_command("help")
 @client.event
 async def on_ready():
   print('Bot is ready!')
@@ -32,7 +33,7 @@ async def lyr(ctx, *arg):
     a = a + " " + str
 
   a
-
+  lyrics_str=''
   s_ = string[string.index('~') : len(string)]
   for _s in s_ :
     if(_s == '~'):
@@ -50,10 +51,32 @@ async def lyr(ctx, *arg):
           if line == '':
             lyrics.remove(line)
           else:
-            await ctx.channel.send("{}".format(line))
+            lyrics_str+=line
+            #await ctx.channel.send("{}".format(line))
+      await ctx.channel.send(lyrics_str)
       #await ctx.channel.send("Here's a link to the annotated lyrics: \n{}".format(lyrics))
   else:
         await ctx.channel.send("I was unable to find the queried song. My apologies :(  " +
         "Check for typos and try again.")
+
+
+
+  
+#for help
+@client.command()
+async def help(ctx):
+    embed= discord.Embed(
+        title = 'Lyricist command list',
+        description = 'Here you can find a list of commands you can use.',
+        colour = discord.Colour.random() 
+    )
+    embed.add_field(name='.help',value='Use this command to get help about how to use the bot.',inline = True)
+    embed.add_field(name='.lyr',value='Use this command to get lyrics of songs.(used as -lyr artist_name ~ song_name)',inline = True)
+    embed.set_thumbnail(url='https://picsum.photos/200/300')
+    #embed.add_field(name='',value='',inline = False)
+    #embed.add_field(name='',value='',inline = True)
+    #embed.set_footer(name = 'Requested by', value = 'member.name')
+
+    await ctx.channel.send(embed=embed) #ctx.send
 
 client.run(TOKEN)
